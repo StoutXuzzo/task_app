257f62e35049ab86ad1d4b15ce08e9dd3b748814
Josep <stoutxuzzo@gmail.com>
2021-12-22 09:05:34 +0100

    Ading modify date and constrains

--- models/task_model.py
+++ models/task_model.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from datetime import datetime
 
 
 class TaskModel(models.Model):
     _name = 'task_app.task_model'
     _description = 'Task Model' 
+    _sql_constraints = [('task_name_uniq', 'UNIQUE (name, active)', 'There cannot be two tasks with the same name!!'),]
 
     name = fields.Char("Task Name", required = True)
     is_done = fields.Boolean("Is done?", default = False)
     active = fields.Boolean("Is active?", default = True)
     category = fields.Many2one("task_app.category_model", "Category")
+
+    date = fields.Datetime("Last change", compute="_lastChange", store=True)
 
     def changeState(self):
         self.ensure_one()
@@ -22,3 +27,16 @@ class TaskModel(models.Model):
         data = self.search([("active", "=", False)])
         for obj in data:
             obj.unlink()
+
+    @api.constrains("name")
+    def _check_length(self):
+        if len(self.name) < 5:
+            raise ValidationError("The name of the task must have at least 5 characters!")
+        return True
+
+    @api.depends("name", "is_done", "active", "category")
+    def _lastChange(self):
+        self.date = datetime.now()
+        return True
+
+    
